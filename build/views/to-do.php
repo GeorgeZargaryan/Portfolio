3652ca3d5fd97873f80b204d72952890de4c44cb
GeorgeZargaryan <georgezargaryan7@gmail.com>
2022-07-30 22:26:57 +0400

    start

---
+++ build/views/to-do.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+require_once("../model/db.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../style/style.css">
+    <title>To-Do | Portfolio</title>
+
+</head>
+<body>
+    <section>
+        <header class="headerWrap">
+            <div class="container navbarList">
+            <h1 class="logoDemo">To-Do.php</h1>
+            <a href="index.php">Return</a>
+            </div>
+        </header>
+    </section>
+    
+    
+</body>
+</html>
